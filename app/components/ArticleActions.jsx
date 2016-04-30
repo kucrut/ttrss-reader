d2e1509b0aae33d15de09940c80059c343343986
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 01:44:35 +0800

    Lint

--- app/components/ArticleActions.jsx
+++ app/components/ArticleActions.jsx
@@ -1,6 +1,7 @@
-import React, { PropTypes } from 'react'
-import { connect }          from 'react-redux'
-import { updateArticle }    from '../actions/articles'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { updateArticle } from 'actions/articles';
+
 
 class ArticleActions extends React.Component {
 	static propTypes = {
@@ -9,53 +10,53 @@ class ArticleActions extends React.Component {
 	}
 
 	constructor( props ) {
-		super( props )
+		super( props );
 		this.state = {
 			unread: false,
 			marked: false
-		}
-	}
-
-	handleClickAction( action ) {
-		const { article, dispatch } = this.props
-
-		this.setState({ [ action]: ! this.state[ action ] } )
-		dispatch( updateArticle( article.id, action, 2 ) )
-	}
-
-	updateState( props ) {
-		const { unread, marked } = props.article
-
-		this.setState({
-			unread: unread,
-			marked: marked
-		})
+		};
 	}
 
 	componentWillMount() {
-		this.updateState( this.props )
+		this.updateState( this.props );
 	}
 
 	componentWillReceiveProps( nextProps ) {
-		this.updateState( nextProps )
+		this.updateState( nextProps );
+	}
+
+	handleClickAction( action ) {
+		const { article, dispatch } = this.props;
+
+		this.setState({ [ action ]: ! this.state[ action ] });
+		dispatch( updateArticle( article.id, action, 2 ) );
+	}
+
+	updateState( props ) {
+		const { unread, marked } = props.article;
+
+		this.setState({
+			unread,
+			marked
+		});
 	}
 
 	render() {
-		const { unread, marked } = this.state
+		const { unread, marked } = this.state;
 
-		let starTitle = marked ? 'Unstar' : 'Star'
-		let starIcon  = marked ? 'fa-star' : 'fa-star-empty'
+		let starTitle = marked ? 'Unstar' : 'Star';
+		let starIcon  = marked ? 'fa-star' : 'fa-star-empty';
 
-		let readTitle = unread ? 'Mark as read' : 'Mark as unread'
-		let readIcon  = unread ? 'fa-check-empty' : 'fa-check'
+		let readTitle = unread ? 'Mark as read' : 'Mark as unread';
+		let readIcon  = unread ? 'fa-check-empty' : 'fa-check';
 
 		return (
 			<div className="actions">
-				<a onClick={ () => { this.handleClickAction( 'unread' ) } } title={ readTitle } className={ readIcon } />
-				<a onClick={ () => { this.handleClickAction( 'marked' ) } } title={ starTitle } className={ starIcon } />
+				<a onClick={ () => { this.handleClickAction( 'unread' ); } } title={ readTitle } className={ readIcon } />
+				<a onClick={ () => { this.handleClickAction( 'marked' ); } } title={ starTitle } className={ starIcon } />
 			</div>
-		)
+		);
 	}
 }
 
-export default connect()( ArticleActions )
+export default connect()( ArticleActions );
