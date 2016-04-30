c69a1bea8afe6ca9ce8a92d1832e1224b350760b
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 02:29:01 +0800

    Lint

--- app/mixins/MenuToggle.jsx
+++ app/mixins/MenuToggle.jsx
@@ -1,10 +1,8 @@
-import React           from 'react'
-import { hideSidebar } from '../actions/ui'
+import React from 'react';
+import { hideSidebar } from 'actions/ui';
 
-class MenuToggle extends React.Component {
+export default class MenuToggle extends React.Component {
 	handleClickHide() {
-		this.props.dispatch( hideSidebar() )
+		this.props.dispatch( hideSidebar() );
 	}
 }
-
-export default ( MenuToggle )
