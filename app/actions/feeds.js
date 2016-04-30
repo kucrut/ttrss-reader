ff5cbe467f5739f24ec02c5252ae47c0f21ba897
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 04:18:24 +0800

    Lint

--- app/actions/feeds.js
+++ app/actions/feeds.js
@@ -1,24 +1,30 @@
-import Api               from '../api'
-import { clearArticles } from './articles'
+import Api from 'api';
+import { find } from 'lodash';
+import { clearArticles } from 'actions/articles';
 
-export const RECEIEVED_FEEDS = 'RECEIEVED_FEEDS'
-export const SELECTED_FEED = 'SELECTED_FEED'
 
-export function fetchFeeds( category ) {
-	return function( dispatch, getState ) {
-		const { session, settings, categories } = getState()
-		const { unreadOnly } = settings
-		const { url, sid } = session
+export const RECEIEVED_FEEDS = 'RECEIEVED_FEEDS';
+export const SELECTED_FEED = 'SELECTED_FEED';
+
+
+export function fetchFeeds( cat ) {
+	return ( dispatch, getState ) => {
+		const { session, settings, categories } = getState();
+		const { unreadOnly } = settings;
+		const { url, sid } = session;
+		let category;
 
 		if ( 'number' === typeof category ) {
-			category = _.find( categories.items, { id: category } )
+			category = find( categories.items, { id: cat });
+		} else {
+			category = cat;
 		}
 
 		return Api.request( url, {
 			op:          'getFeeds',
-			sid:         sid,
 			cat_id:      category.id,
-			unread_only: 0 < unreadOnly
+			unread_only: 0 < unreadOnly,
+			sid
 		})
 			.then( response => response.json() )
 			.then( json => {
@@ -26,18 +32,19 @@ export function fetchFeeds( category ) {
 					type:  RECEIEVED_FEEDS,
 					items: json.content,
 					category
-				})
-			})
-	}
+				});
+			});
+	};
 }
 
 export function selectFeed( id ) {
-	return function( dispatch ) {
+	return ( dispatch ) => {
 		dispatch({
 			type: SELECTED_FEED,
 			id
-		})
-		dispatch( clearArticles( id ) )
-	}
+		});
+
+		dispatch( clearArticles( id ) );
+	};
 }
 
