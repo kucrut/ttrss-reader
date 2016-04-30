2e960864f1ac0ddf36b5080da37ed529c164b0a0
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-30 21:01:48 +0800

    Lint

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,16 @@
-import React                            from 'react'
-import { render }                       from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware                  from 'redux-thunk'
-import { Provider }                     from 'react-redux'
-import reducers                         from './reducers'
-import App                              from './containers/App'
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import App from './containers/App';
 
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware
-)( createStore )
+)( createStore );
 
-const store = createStoreWithMiddleware( reducers )
+const store = createStoreWithMiddleware( reducers );
 
 render(
 	<Provider store={ store }>
