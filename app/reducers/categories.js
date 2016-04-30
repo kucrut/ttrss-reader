471c74e3b8097b73f90d34e892d23e84733df2e6
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-30 23:43:05 +0800

    Lint

--- app/reducers/categories.js
+++ app/reducers/categories.js
@@ -1,17 +1,18 @@
 import { PropTypes } from 'react';
-import _ from 'lodash'
-import { REQUESTED_LOGOUT } from '../actions/session'
+import _ from 'lodash';
+import { REQUESTED_LOGOUT } from 'actions/session';
 import {
 	REQUESTED_CATEGORIES,
 	RECEIEVED_CATEGORIES,
 	REQUESTED_ALL_CATEGORIES,
 	RECEIEVED_ALL_CATEGORIES
-} from '../actions/categories'
+} from 'actions/categories';
+
 
 const initialState = {
 	isFetching: true,
 	items:      []
-}
+};
 
 export const categoriesShape = {
 	isFetching: PropTypes.bool,
@@ -22,64 +23,65 @@ function normalizeCategory( category ) {
 	return Object.assign({}, category, {
 		id:     parseInt( category.id, 10 ),
 		unread: parseInt( category.unread, 10 )
-	})
+	});
 }
 
 export function categories( state = initialState, action ) {
+	let feedCategories;
+	let specialIdx;
+
 	switch ( action.type ) {
 		case REQUESTED_CATEGORIES:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				isFetching: action.isFetching
-			})
+			});
 
 		case RECEIEVED_CATEGORIES:
-			let categories = action.categories.map( normalizeCategory )
-			let specialIdx = _.findIndex( categories, { id: -1 } )
+			feedCategories = action.categories.map( normalizeCategory );
+			specialIdx     = _.findIndex( feedCategories, { id: -1 });
 
 			if ( 0 < specialIdx ) {
-				categories.splice( 0, 0, categories.splice( specialIdx, 1 )[0] )
+				feedCategories.splice( 0, 0, feedCategories.splice( specialIdx, 1 )[ 0 ] );
 			}
 
-			return Object.assign( {}, state, {
-				isFetching: false,
-				items: categories
-			})
+			return Object.assign({}, state, {
+				items:      feedCategories,
+				isFetching: false
+			});
 
 		case REQUESTED_LOGOUT:
-			return Object.assign( {}, initialState )
+			return Object.assign({}, initialState );
 
 		default:
-			return state
+			return state;
 	}
 }
 
-const _allCategories = {
-	isFetching: false,
-	items: []
-}
+const initialAllCategories = {
+	items:      [],
+	isFetching: false
+};
 
-export function allCategories( state = _allCategories, action ) {
-	let categories
+export function allCategories( state = initialAllCategories, action ) {
+	let feedCategories;
 
 	switch ( action.type ) {
 		case REQUESTED_ALL_CATEGORIES:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				isFetching: action.isFetching
-			})
+			});
 
 		case RECEIEVED_ALL_CATEGORIES:
-			categories = action.categories.map( normalizeCategory )
-			categories = _.filter( categories, ( item ) => {
-				return -1 < item.id
-			})
-			categories = _.sortBy( categories, 'title' )
+			feedCategories = action.feedCategories.map( normalizeCategory );
+			feedCategories = _.filter( feedCategories, ( item ) => -1 < item.id );
+			feedCategories = _.sortBy( feedCategories, 'title' );
 
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				isFetching: false,
-				items:      categories
-			})
+				items:      feedCategories
+			});
 
 		default:
-			return state
+			return state;
 	}
 }
