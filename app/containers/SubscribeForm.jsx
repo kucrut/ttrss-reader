d41088b2a94a3bc65115401f4c9fa6fd0e651c47
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 01:27:19 +0800

    Lint

--- app/containers/SubscribeForm.jsx
+++ app/containers/SubscribeForm.jsx
@@ -1,8 +1,9 @@
-import React, { PropTypes } from 'react'
-import { connect }          from 'react-redux'
-import _                    from 'lodash'
-import { getAllCategories } from '../actions/categories'
-import { subscribeToFeed, closeSubscriptionForm }  from '../actions/subscription'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { getAllCategories } from 'actions/categories';
+import { subscribeToFeed, closeSubscriptionForm } from 'actions/subscription';
+
 
 class SubscribeForm extends React.Component {
 	static propTypes = {
@@ -12,145 +13,156 @@ class SubscribeForm extends React.Component {
 	}
 
 	constructor( props ) {
-		super( props )
+		super( props );
 
 		this.state = {
 			url:      '',
 			category: 0,
 			feeds:    []
-		}
+		};
 
 		this.submitForm = this.submitForm.bind( this );
 		this.handleChange = this.handleChange.bind( this );
 		this.handleClickClose = this.handleClickClose.bind( this );
 	}
 
-	submitForm( e ) {
-		const { url, category } = this.state
-
-		e.preventDefault()
-		this.props.dispatch( subscribeToFeed( url, category ) )
-	}
-
-	handleChange( e ) {
-		this.setState({ [ e.target.name ]: e.target.value })
-	}
-
-	handleClickClose() {
-		this.props.dispatch( closeSubscriptionForm() )
-	}
-
 	componentDidMount() {
-		this.props.dispatch( getAllCategories() )
+		this.props.dispatch( getAllCategories() );
 	}
 
 	componentWillReceiveProps( nextProps ) {
-		const { url }    = this.state
-		const { status } = nextProps.subscription
+		const { url }    = this.state;
+		const { status } = nextProps.subscription;
 
-		let feeds, feedsUrls, state = {}
+		let feeds;
+		let feedsUrls;
+		let state = {};
 
 		switch ( status.code ) {
 			case 0:
 			case 1:
-				state.url   = ''
-				state.feeds = []
-				break
+				state = {
+					url:   '',
+					feeds: []
+				};
+				break;
 
 			case 4:
-				feedsUrls = _.keys( status.feeds )
-				feeds = feedsUrls.map( url => {
-					return {
-						url:   url,
-						title: status.feeds[ url ]
-					}
-				})
+				feedsUrls = _.keys( status.feeds );
+				feeds     = feedsUrls.map( feedUrl => ({
+					url:   feedUrl,
+					title: status.feeds[ url ]
+				}) );
 
-				state.feeds = feeds
+				state.feeds = feeds;
 				if ( 0 > feedsUrls.indexOf( url ) ) {
-					state.url = feedsUrls[0]
+					state.url = feedsUrls[ 0 ];
 				}
-				break
+				break;
 
 			default:
-				state.feeds = []
+				state.feeds = [];
 		}
 
-		this.setState( state )
+		this.setState( state );
+	}
+
+	handleChange( e ) {
+		this.setState({ [ e.target.name ]: e.target.value });
+	}
+
+	handleClickClose() {
+		this.props.dispatch( closeSubscriptionForm() );
+	}
+
+	submitForm( e ) {
+		const { url, category } = this.state;
+
+		e.preventDefault();
+		this.props.dispatch( subscribeToFeed( url, category ) );
 	}
 
 	renderMessage() {
-		const { message } = this.props.subscription
+		const { message } = this.props.subscription;
+		let element;
 
 		if ( message ) {
-			return (
+			element = (
 				<p className="message">{ message }</p>
-			)
+			);
 		}
+
+		return element;
 	}
 
 	renderUrlField() {
-		const { url, feeds } = this.state
-		const { status }     = this.props.subscription
-		let element, feedUrls
+		const { url, feeds } = this.state;
+		let element;
 
 		if ( feeds.length ) {
 			element = (
 				<select id="s-url" name="url" value={ url } onChange={ this.handleChange }>
-					{ feeds.map( feed => {
-						return <option key={ feed.url } value={ feed.url }>{ feed.title }</option>
-					}) }
+					{ feeds.map( feed =>
+						<option key={ feed.url } value={ feed.url }>{ feed.title }</option>
+					) }
 				</select>
-			)
+			);
 		} else {
 			element = (
-				<input id="s-url" type="url" name="url" required value={ url } onChange={ this.handleChange } />
-			)
+				<input
+					id="s-url"
+					type="url"
+					name="url"
+					required
+					value={ url }
+					onChange={ this.handleChange }
+				/>
+			);
 		}
 
-		return element
+		return element;
 	}
 
 	renderCategories() {
-		const { category }      = this.state
-		const { allCategories } = this.props
-		let element
+		const { category }      = this.state;
+		const { allCategories } = this.props;
+		let element;
 
 		if ( allCategories.isFetching ) {
 			element = (
-				<label className="iwrap"><i className="fa-spinner animate-spin" /> Loading Categories</label>
-			)
+				<label className="iwrap">
+					<i className="fa-spinner animate-spin" /> Loading Categories
+				</label>
+			);
 		} else {
 			element = (
 				<select id="s-category" name="category" value={ category } onChange={ this.handleChange }>
-					{ allCategories.items.map( item => {
-						return <option key={ item.id } value={ item.id }>{ item.title }</option>
-					}) }
+					{ allCategories.items.map( item =>
+						<option key={ item.id } value={ item.id }>{ item.title }</option>
+					) }
 				</select>
-			)
+			);
 		}
 
-		return element
+		return element;
 	}
 
 	renderButton() {
-		const { url }           = this.state
-		const { isSubscribing } = this.props.subscription
+		const { url }           = this.state;
+		const { isSubscribing } = this.props.subscription;
+		const classes = isSubscribing ? 'fa-spinner animate-spin' : 'fa-eye';
 
-		let classes = isSubscribing ? 'fa-spinner animate-spin' : 'fa-eye'
-		let attrs   = {}
+		let attrs   = {};
 		if ( ! url || isSubscribing ) {
-			attrs.disabled = 'disabled'
+			attrs = { disabled: 'disabled' };
 		}
 
 		return (
 			<button type="submit" { ...attrs }><i className={ classes } /> Subscribe</button>
-		)
+		);
 	}
 
 	render() {
-		const { url } = this.state
-
 		return (
 			<form className="login-form" onSubmit={ this.submitForm }>
 				<h1><i className="fa-rss" /> Subscribe to Feed</h1>
@@ -169,7 +181,7 @@ class SubscribeForm extends React.Component {
 
 				<a className="fa-cancel close" title="Close" onClick={ this.handleClickClose } />
 			</form>
-		)
+		);
 	}
 }
 
@@ -177,7 +189,7 @@ function mapStateToProps( state ) {
 	return {
 		allCategories: state.allCategories,
 		subscription:  state.subscription
-	}
+	};
 }
 
-export default connect( mapStateToProps )( SubscribeForm )
+export default connect( mapStateToProps )( SubscribeForm );
