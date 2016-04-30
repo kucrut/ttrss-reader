01c27fcc7a3a968b61053a7b95f2a25df386268d
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 01:10:00 +0800

    Lint

--- app/containers/FeedList.jsx
+++ app/containers/FeedList.jsx
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchFeeds } from 'actions/feeds';
-import FeedItem from '../components/FeedItem';
-import Spinner from '../components/Spinner';
+import FeedItem from 'components/FeedItem';
+import Spinner from 'components/Spinner';
 
 
 class FeedList extends React.Component {
