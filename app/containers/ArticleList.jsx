6093c0e42b56be50f82ab1490a8700b33d20537f
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 01:08:28 +0800

    Lint

--- app/containers/ArticleList.jsx
+++ app/containers/ArticleList.jsx
@@ -1,68 +1,70 @@
-import React, { PropTypes }       from 'react'
-import { connect }                from 'react-redux'
-import classNames                 from 'classnames'
-import _                          from 'lodash'
-import { fetchFeedArticles }      from '../actions/articles'
-import { setArticleListPosition } from '../actions/ui'
-import Spinner                    from '../components/Spinner'
-import Article                    from '../components/Article'
-import FeedActions                from '../components/FeedActions'
-import LoadMoreButton             from '../components/LoadMoreButton'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchFeedArticles } from 'actions/articles';
+import { setArticleListPosition } from 'actions/ui';
+import Spinner from 'components/Spinner';
+import Article from 'components/Article';
+import FeedActions from 'components/FeedActions';
+import LoadMoreButton from 'components/LoadMoreButton';
+
 
 class ArticleList extends React.Component {
-	fetchArticles( feed ) {
-		this.props.dispatch( fetchFeedArticles( feed ) )
-	}
-
-	componentWillReceiveProps( nextProps ) {
-		const { feed, articles, dispatch } = nextProps
-
-		if ( feed.id !== this.props.feed.id && ! articles.isFetching ) {
-			this.fetchArticles( feed )
-		}
-	}
-
 	componentDidMount() {
-		const { feed, articles, scrollPos } = this.props
+		const { feed, articles, scrollPos } = this.props;
 
 		if ( ! articles.items.length ) {
-			this.fetchArticles( feed )
+			this.fetchArticles( feed );
 		} else {
 			if ( scrollPos.feedId === feed.id ) {
-				document.getElementsByClassName( 'content' )[0].scrollTop = scrollPos.top
+				document.getElementsByClassName( 'content' )[ 0 ].scrollTop = scrollPos.top;
 			}
 		}
 	}
 
-	componentWillUnmount() {
-		const { feed, dispatch } = this.props
+	componentWillReceiveProps( nextProps ) {
+		const { feed, articles } = nextProps;
 
-		dispatch( setArticleListPosition({
-			feedId: feed.id,
-			top:    document.getElementsByClassName( 'content' )[0].scrollTop
-		}) )
-	}
-
-	renderLoadMoreButton() {
-		const { items, hasMore } = this.props.articles
-
-		if ( items.length && hasMore ) {
-			return (
-				<LoadMoreButton { ...this.props } />
-			)
+		if ( feed.id !== this.props.feed.id && ! articles.isFetching ) {
+			this.fetchArticles( feed );
 		}
 	}
 
+	componentWillUnmount() {
+		const { feed, dispatch } = this.props;
+
+		dispatch( setArticleListPosition({
+			feedId: feed.id,
+			top:    document.getElementsByClassName( 'content' )[ 0 ].scrollTop
+		}) );
+	}
+
+	fetchArticles( feed ) {
+		this.props.dispatch( fetchFeedArticles( feed ) );
+	}
+
+	renderLoadMoreButton() {
+		const { items, hasMore } = this.props.articles;
+		let button;
+
+		if ( items.length && hasMore ) {
+			button = (
+				<LoadMoreButton { ...this.props } />
+			);
+		}
+
+		return button;
+	}
+
 	renderList() {
-		const { items } = this.props.articles
+		const { items } = this.props.articles;
 
 		return (
 			<div className="articles">
 				{ items.map( article => (
 					<Article key={ article.id } article={ article } isSingle={ false } />
-				)) }
+				) ) }
 			</div>
-		)
+		);
 	}
 
 	renderNoArticles() {
@@ -70,32 +72,36 @@ class ArticleList extends React.Component {
 			<article className="nothing-found">
 				<p><em>No articles found.</em></p>
 			</article>
-		)
+		);
 	}
 
 	renderSpinner() {
 		return (
 			<div className="main-init"><Spinner /></div>
-		)
+		);
 	}
 
 	renderFeedActions() {
+		let actions;
+
 		if ( this.props.articles.items.length ) {
-			return ( <FeedActions /> )
+			actions = ( <FeedActions /> );
 		}
+
+		return actions;
 	}
 
 	render() {
-		const { isFetching, items } = this.props.articles
-		let content
+		const { isFetching, items } = this.props.articles;
+		let content;
 
 		if ( items.length ) {
-			content = this.renderList()
+			content = this.renderList();
 		} else {
 			if ( isFetching ) {
-				content = this.renderSpinner()
+				content = this.renderSpinner();
 			} else {
-				content = this.renderNoArticles()
+				content = this.renderNoArticles();
 			}
 		}
 
@@ -104,7 +110,7 @@ class ArticleList extends React.Component {
 				{ content }
 				{ this.renderLoadMoreButton() }
 			</div>
-		)
+		);
 	}
 }
 
@@ -114,14 +120,14 @@ ArticleList.propTypes = {
 	articles:   PropTypes.object.isRequired,
 	unreadOnly: PropTypes.number.isRequired,
 	dispatch:   PropTypes.func.isRequired
-}
+};
 
 function mapStateToProps( state ) {
 	return {
 		scrollPos:  state.ui.articleListPosition,
 		articles:   state.articles,
 		unreadOnly: state.settings.unreadOnly
-	}
+	};
 }
 
-export default connect( mapStateToProps )( ArticleList )
+export default connect( mapStateToProps )( ArticleList );
