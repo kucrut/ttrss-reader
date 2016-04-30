59ec93190f3e45d849117cab96640d281bf17457
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 03:56:36 +0800

    Lint

--- app/actions/categories.js
+++ app/actions/categories.js
@@ -1,72 +1,68 @@
-import Api        from '../api'
-import { addLog } from './log'
+import Api from 'api';
+import { addLog } from 'actions/log';
 
-export const REQUESTED_CATEGORIES = 'REQUESTED_CATEGORIES'
-export const RECEIEVED_CATEGORIES = 'RECEIEVED_CATEGORIES'
-export const REQUESTED_ALL_CATEGORIES = 'REQUESTED_ALL_CATEGORIES'
-export const RECEIEVED_ALL_CATEGORIES = 'RECEIEVED_ALL_CATEGORIES'
+export const REQUESTED_CATEGORIES = 'REQUESTED_CATEGORIES';
+export const RECEIEVED_CATEGORIES = 'RECEIEVED_CATEGORIES';
+export const REQUESTED_ALL_CATEGORIES = 'REQUESTED_ALL_CATEGORIES';
+export const RECEIEVED_ALL_CATEGORIES = 'RECEIEVED_ALL_CATEGORIES';
 
 function logError( dispatch, error ) {
 	dispatch( addLog({
 		source:  'categories',
 		type:    'error',
 		message: error.message
-	}) )
+	}) );
 
 	dispatch({
-		type: REQUESTED_CATEGORIES,
+		type:       REQUESTED_CATEGORIES,
 		isFetching: false
-	})
+	});
 }
 
 export function getCategories() {
-	return function( dispatch, getState ) {
-		const { session, settings } = getState()
-		const { unreadOnly } = settings
-		const { url, sid } = session
+	return ( dispatch, getState ) => {
+		const { session, settings } = getState();
+		const { unreadOnly } = settings;
+		const { url, sid } = session;
 
 		dispatch({
-			type: REQUESTED_CATEGORIES,
+			type:       REQUESTED_CATEGORIES,
 			isFetching: true
-		})
+		});
 
 		return Api.request( url, {
 			op:          'getCategories',
-			sid:         sid,
-			unread_only: 0 < unreadOnly
+			unread_only: 0 < unreadOnly,
+			sid
 		})
 			.then( response => response.json() )
-			.then( json => {
-				dispatch({
-					type: RECEIEVED_CATEGORIES,
-					categories: json.content
-				})
-			})
-			.catch( err => logError( dispatch, err ) )
-	}
+			.then( json => dispatch({
+				type:       RECEIEVED_CATEGORIES,
+				categories: json.content
+			}) )
+			.catch( err => logError( dispatch, err ) );
+	};
 }
 
 export function getAllCategories() {
-	return function( dispatch, getState ) {
-		const { url, sid } = getState().session
+	return ( dispatch, getState ) => {
+		const { url, sid } = getState().session;
 
 		dispatch({
-			type: REQUESTED_ALL_CATEGORIES,
+			type:       REQUESTED_ALL_CATEGORIES,
 			isFetching: true
-		})
+		});
 
 		return Api.request( url, {
 			op:            'getCategories',
-			sid:           sid,
-			include_empty: true
+			include_empty: true,
+			sid
 		})
 			.then( response => response.json() )
-			.then( json => {
-				dispatch({
-					type: RECEIEVED_ALL_CATEGORIES,
-					categories: json.content
-				})
-			})
-			.catch( err => logError( dispatch, err ) )
-	}
+			.then( json => dispatch({
+				type:       RECEIEVED_ALL_CATEGORIES,
+				categories: json.content
+			}) )
+			.catch( err => logError( dispatch, err ) );
+	};
 }
