aee0c1f12f9c086bf5e92eacf63b0eea8e122426
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 04:20:38 +0800

    Lint

--- app/actions/settings.js
+++ app/actions/settings.js
@@ -1,42 +1,41 @@
-import { getCategories } from './categories'
+export const OPENED_SETTINGS_FORM = 'OPENED_SETTINGS_FORM';
+export const UPDATED_SETTINGS = 'UPDATED_SETTINGS';
+export const UPDATED_SORT_ORDER = 'UPDATED_SORT_ORDER';
+export const CLOSED_SETTINGS_FORM = 'CLOSED_SETTINGS_FORM';
 
-export const OPENED_SETTINGS_FORM = 'OPENED_SETTINGS_FORM'
-export const UPDATED_SETTINGS = 'UPDATED_SETTINGS'
-export const UPDATED_SORT_ORDER = 'UPDATED_SORT_ORDER'
-export const CLOSED_SETTINGS_FORM = 'CLOSED_SETTINGS_FORM'
 
 export function openSettingsForm() {
-	return function( dispatch ) {
-		dispatch({ type: OPENED_SETTINGS_FORM })
-	}
+	return ( dispatch ) => {
+		dispatch({ type: OPENED_SETTINGS_FORM });
+	};
 }
 
 export function closeSettingsForm() {
-	return function( dispatch ) {
-		dispatch({ type: CLOSED_SETTINGS_FORM })
-	}
+	return ( dispatch ) => {
+		dispatch({ type: CLOSED_SETTINGS_FORM });
+	};
 }
 
 export function saveSettings( settings ) {
-	localStorage.setItem( 'ttrssSettingUnreadOnly', settings.unreadOnly )
-	localStorage.setItem( 'ttrssSettingLimit', settings.limit )
-	localStorage.setItem( 'ttrssSettingNoPagination', settings.noPagination )
+	localStorage.setItem( 'ttrssSettingUnreadOnly', settings.unreadOnly );
+	localStorage.setItem( 'ttrssSettingLimit', settings.limit );
+	localStorage.setItem( 'ttrssSettingNoPagination', settings.noPagination );
 
-	return function( dispatch ) {
+	return ( dispatch ) => {
 		dispatch({
 			type: UPDATED_SETTINGS,
 			settings
-		})
-	}
+		});
+	};
 }
 
 export function updateSortOrder( dateReverse ) {
-	localStorage.setItem( 'ttrssSettingDateReverse', dateReverse )
+	localStorage.setItem( 'ttrssSettingDateReverse', dateReverse );
 
-	return function( dispatch ) {
+	return ( dispatch ) => {
 		dispatch({
 			type: UPDATED_SORT_ORDER,
 			dateReverse
-		})
-	}
+		});
+	};
 }
