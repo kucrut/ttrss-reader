fcd936af012a9bddedbff43c5c389986496e7e0f
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 05:05:12 +0800

    Lint

--- app/reducers/subscription.js
+++ app/reducers/subscription.js
@@ -3,7 +3,8 @@ import {
 	REQUESTED_SUBSCRIPTION,
 	RECEIVED_SUBSCRIPTION,
 	CLOSED_SUBSCRIPTION
-} from '../actions/subscription'
+} from 'actions/subscription';
+
 
 const messages = [
 	'Feed already exists.',
@@ -12,36 +13,36 @@ const messages = [
 	'URL content is HTML, no feeds available.',
 	'Please select a feed to subscribe to:',
 	"Couldn't download the URL content."
-]
+];
 
 const initialState = {
 	isOpen:        false,
 	isSubscribing: false,
 	status:        {},
 	message:       ''
-}
+};
 
 export default function subscription( state = initialState, action ) {
 	switch ( action.type ) {
 		case OPENED_SUBSCRIPTION:
-			return Object.assign( {}, initialState, { isOpen: true })
+			return Object.assign({}, initialState, { isOpen: true });
 
 		case REQUESTED_SUBSCRIPTION:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				isSubscribing: action.isSubscribing
-			})
+			});
 
 		case RECEIVED_SUBSCRIPTION:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				isSubscribing: false,
 				status:        action.status,
 				message:       messages[ action.status.code ]
-			})
+			});
 
 		case CLOSED_SUBSCRIPTION:
-			return Object.assign( {}, initialState )
+			return Object.assign({}, initialState );
 
 		default:
-			return state
+			return state;
 	}
 }
