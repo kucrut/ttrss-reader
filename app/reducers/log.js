3ec51a613b6a73ae945d4d2812c4fab44d993d0d
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-30 23:00:25 +0800

    Lint

--- app/reducers/log.js
+++ app/reducers/log.js
@@ -1,9 +1,9 @@
 import { PropTypes } from 'react';
-import { ADDED_LOG, REMOVED_LOG } from '../actions/log'
+import { ADDED_LOG, REMOVED_LOG } from 'actions/log';
 
 const initialState = {
 	item: {}
-}
+};
 
 export const logsShape = {
 	item: PropTypes.object
@@ -12,16 +12,16 @@ export const logsShape = {
 export default function log( state = initialState, action ) {
 	switch ( action.type ) {
 		case ADDED_LOG:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				item: action.item
-			})
+			});
 
 		case REMOVED_LOG:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				item: initialState.item
-			})
+			});
 
 		default:
-			return state
+			return state;
 	}
 }
