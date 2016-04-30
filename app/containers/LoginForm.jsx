2f78e3a9bcc0279ea3ad3be4aa997f889c68af11
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 01:04:03 +0800

    Lint

--- app/containers/LoginForm.jsx
+++ app/containers/LoginForm.jsx
@@ -1,16 +1,15 @@
-import React, { PropTypes } from 'react'
-import { connect }          from 'react-redux'
-import _                    from 'lodash'
-import { requestLogin }     from '../actions/session'
+import React, { PropTypes } from 'react';
+import { requestLogin } from 'actions/session';
 
-class LoginForm extends React.Component {
+
+export default class LoginForm extends React.Component {
 	static propTypes = {
 		isAsking: PropTypes.bool.isRequired,
 		dispatch: PropTypes.func.isRequired
 	}
 
 	constructor( props ) {
-		super( props )
+		super( props );
 
 		this.state = {
 			user:     '',
@@ -23,55 +22,76 @@ class LoginForm extends React.Component {
 	}
 
 	handleChange( e ) {
-		this.setState({ [ e.target.name ]: e.target.value })
+		this.setState({ [ e.target.name ]: e.target.value });
 	}
 
 	submitForm( e ) {
-		const { dispatch } = this.props
+		const { dispatch } = this.props;
 
-		e.preventDefault()
-		dispatch( requestLogin( this.state ) )
+		e.preventDefault();
+		dispatch( requestLogin( this.state ) );
 	}
 
 	renderButton() {
-		let iconClass, attrs = {}
+		let iconClass;
+		let attrs = {};
 
 		if ( this.props.isAsking ) {
-			attrs.disabled = 'disabled'
-			iconClass = 'fa-spinner animate-spin'
+			attrs     = { disabled: 'disabled' };
+			iconClass = 'fa-spinner animate-spin';
 		} else {
-			iconClass = 'fa-login'
+			iconClass = 'fa-login';
 		}
 
 		return (
 			<button { ...attrs }><i className={ iconClass } /> Login</button>
-		)
+		);
 	}
 
 	render() {
-		const { user, password, url } = this.state
+		const { user, password, url } = this.state;
 
 		return (
-			<form className="login-form" onSubmit={ this.submitForm.bind( this ) }>
+			<form className="login-form" onSubmit={ this.submitForm }>
 				<h1><i className="fa-rss" /> Tiny Tiny RSS Reader</h1>
 				<div className="form-row">
 					<label htmlFor="login-user">Username</label>
-					<input id="login-user" type="text" name="user" value={ user } onChange={ this.handleChange } required autofocus  />
+					<input
+						id="login-user"
+						type="text"
+						name="user"
+						value={ user }
+						onChange={ this.handleChange }
+						required
+						autoFocus
+					/>
 				</div>
 				<div className="form-row">
 					<label htmlFor="login-password">Password</label>
-					<input id="login-password" type="password" name="password" value={ password } onChange={ this.handleChange } required />
+					<input
+						id="login-password"
+						type="password"
+						name="password"
+						value={ password }
+						onChange={ this.handleChange }
+						required
+					/>
 				</div>
 				<div className="form-row">
 					<label htmlFor="login-url">URL</label>
-					<input id="login-url" type="url" name="url" value={ url } onChange={ this.handleChange } required />
+					<input
+						id="login-url"
+						type="url"
+						name="url"
+						value={ url }
+						onChange={ this.handleChange }
+						required
+					/>
 				</div>
 				<div className="form-row submit-row">
 					{ this.renderButton() }
 				</div>
 			</form>
-		)
+		);
 	}
 }
-
-export default ( LoginForm )
