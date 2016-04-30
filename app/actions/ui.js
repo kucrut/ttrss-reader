52c8abe17db7094f603d2e2e37232a15ee14d046
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 04:39:56 +0800

    Lint

--- app/actions/ui.js
+++ app/actions/ui.js
@@ -1,39 +1,39 @@
-export const SIDEBAR_VISIBILITY = 'SIDEBAR_VISIBILITY'
-export const ARTICLE_LIST_POSITION = 'ARTICLE_LIST_POSITION'
+export const SIDEBAR_VISIBILITY = 'SIDEBAR_VISIBILITY';
+export const ARTICLE_LIST_POSITION = 'ARTICLE_LIST_POSITION';
+
 
 export function showSidebar() {
-	return function( dispatch, getState ) {
+	return ( dispatch ) => {
 		dispatch({
-			type: SIDEBAR_VISIBILITY,
+			type:             SIDEBAR_VISIBILITY,
 			isSidebarVisible: true
-		})
-	}
+		});
+	};
 }
 
 export function hideSidebar() {
-	return function( dispatch, getState ) {
+	return ( dispatch ) => {
 		dispatch({
-			type: SIDEBAR_VISIBILITY,
+			type:             SIDEBAR_VISIBILITY,
 			isSidebarVisible: false
-		})
-	}
+		});
+	};
 }
 
 export function toggleSidebar() {
-	return function( dispatch, getState ) {
+	return ( dispatch, getState ) => {
 		dispatch({
-			type: SIDEBAR_VISIBILITY,
+			type:             SIDEBAR_VISIBILITY,
 			isSidebarVisible: ! getState().ui.isSidebarVisible
-		})
-	}
-
+		});
+	};
 }
 
-export function setArticleListPosition( arg = {} ) {
-	return function( dispatch ) {
+export function setArticleListPosition( arg = {}) {
+	return ( dispatch ) => {
 		dispatch({
-			type: ARTICLE_LIST_POSITION,
+			type:                ARTICLE_LIST_POSITION,
 			articleListPosition: arg
-		})
-	}
+		});
+	};
 }
