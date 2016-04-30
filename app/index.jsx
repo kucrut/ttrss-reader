da94fc44ef91e812f85fbd39928fb8a4f70c383a
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-30 20:51:34 +0800

    Add .editorconfig

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,3 @@
-import 'babel-core/polyfill'
 import React                            from 'react'
 import { render }                       from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
