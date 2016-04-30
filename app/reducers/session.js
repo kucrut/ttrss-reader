0d5381b823991083cb99426fe50a4231158ee6bc
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-30 22:09:37 +0800

    Lint

--- app/reducers/session.js
+++ app/reducers/session.js
@@ -1,12 +1,18 @@
 import { PropTypes } from 'react';
-import { REQUESTED_SESSION, SESSION_CHECKED, RECEIEVED_SID, RECEIEVED_URL, REQUESTED_LOGOUT } from '../actions/session'
+import {
+	REQUESTED_SESSION,
+	SESSION_CHECKED,
+	RECEIEVED_SID,
+	RECEIEVED_URL,
+	REQUESTED_LOGOUT
+} from 'actions/session';
 
 const initialState = {
 	url:       '',
 	sid:       '',
 	isAsking:  false,
 	isChecked: false
-}
+};
 
 export const sessionShape = {
 	url:       PropTypes.string,
@@ -20,36 +26,42 @@ export default function session( state = initialState, action ) {
 		case SESSION_CHECKED:
 			return Object.assign({}, state, {
 				isChecked: action.isChecked
-			})
+			});
 
 		case REQUESTED_SESSION:
 			return Object.assign({}, state, {
 				isAsking: action.isAsking
-			})
+			});
 
 		case RECEIEVED_URL:
 			if ( action.url ) {
-				localStorage.setItem( 'ttrssUrl', action.url )
+				localStorage.setItem( 'ttrssUrl', action.url );
+
 				return Object.assign({}, state, {
 					url: action.url
-				})
+				});
 			}
+
+			return state;
 
 		case RECEIEVED_SID:
 			if ( action.sid ) {
-				localStorage.setItem( 'ttrssSid', action.sid )
+				localStorage.setItem( 'ttrssSid', action.sid );
+
 				return Object.assign({}, state, {
 					sid: action.sid
-				})
+				});
 			}
 
+			return state;
+
 		case REQUESTED_LOGOUT:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				url: initialState.url,
 				sid: initialState.sid
-			} )
+			});
 
 		default:
-			return state
+			return state;
 	}
 }
