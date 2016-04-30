e6864b4528432f0f2d61e51cdbaf2737bcf362c9
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-30 23:49:02 +0800

    Lint

--- app/components/CategoryItem.jsx
+++ app/components/CategoryItem.jsx
@@ -1,44 +1,48 @@
-import React, { PropTypes, Component } from 'react'
-import classNames                      from 'classnames'
-import FeedList                        from '../containers/FeedList'
-import { getCount }                    from '../helpers'
+import React, { PropTypes, Component } from 'react';
+import classNames from 'classnames';
+import FeedList from 'containers/FeedList';
+import { getCount } from 'helpers';
 
-class CategoryItem extends Component{
+
+class CategoryItem extends Component {
 	static propTypes = {
 		category: PropTypes.object.isRequired
 	}
 
 	constructor( props ) {
-		super( props )
-		this.state = { isOpen: false }
+		super( props );
+		this.state = { isOpen: false };
 		this.handleClick = this.handleClick.bind( this );
 	}
 
 	handleClick() {
-		this.setState({ isOpen: ! this.state.isOpen })
+		this.setState({ isOpen: ! this.state.isOpen });
 	}
 
 	renderCount() {
-		const { category } = this.props
+		const { category } = this.props;
+		let count;
 
 		if ( 0 < category.unread ) {
-			return (
+			count = (
 				<span className="count">{ getCount( category.unread ) }</span>
-			)
+			);
 		}
+
+		return count;
 	}
 
 	render() {
-		const { isOpen } = this.state
-		const { category } = this.props
+		const { isOpen } = this.state;
+		const { category } = this.props;
 
-		let itemClass = classNames({ 'is-open': isOpen })
+		let itemClass = classNames({ 'is-open': isOpen });
 		let iconClass = classNames({
-			'fa-folder': ( 0 < category.unread && ! isOpen ),
-			'fa-folder-open': ( 0 < category.unread && isOpen ),
-			'fa-folder-empty': ( 0 === category.unread && ! isOpen ),
+			'fa-folder':            ( 0 < category.unread && ! isOpen ),
+			'fa-folder-open':       ( 0 < category.unread && isOpen ),
+			'fa-folder-empty':      ( 0 === category.unread && ! isOpen ),
 			'fa-folder-open-empty': ( 0 === category.unread && isOpen )
-		})
+		});
 
 		return (
 			<li className={ itemClass } key={ category.id }>
@@ -53,4 +57,4 @@ class CategoryItem extends Component{
 	}
 }
 
-export default ( CategoryItem )
+export default ( CategoryItem );
