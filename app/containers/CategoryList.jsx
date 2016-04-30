60f3fe6e4fdaaee117c19e6727f61a5eabe5c23c
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-30 23:09:07 +0800

    Lint

--- app/containers/CategoryList.jsx
+++ app/containers/CategoryList.jsx
@@ -1,25 +1,30 @@
-import React, { PropTypes } from 'react'
-import { connect }          from 'react-redux'
-import classNames           from 'classnames'
-import { getCategories }    from '../actions/categories'
-import CategoryItem         from '../components/CategoryItem'
-import Spinner              from '../components/Spinner'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { getCategories } from 'actions/categories';
+import CategoryItem from 'components/CategoryItem';
+import Spinner from 'components/Spinner';
+
 
 class CategoryList extends React.Component {
+	static propTypes = {
+		categories: PropTypes.object.isRequired,
+		dispatch:   PropTypes.func.isRequired
+	}
+
 	componentDidMount() {
-		this.props.dispatch( getCategories() )
+		this.props.dispatch( getCategories() );
 	}
 
 	renderSpinner() {
 		return (
 			<li className="placeholder"><Spinner /> Loading Categories</li>
-		)
+		);
 	}
 
 	renderNoCategories() {
 		return (
 			<li className="placeholder">No categories found.</li>
-		)
+		);
 	}
 
 	renderList() {
@@ -27,21 +32,20 @@ class CategoryList extends React.Component {
 			this.props.categories.items.map( category =>
 				<CategoryItem key={ category.id } category={ category } />
 			)
-		)
+		);
 	}
 
 	render() {
-		const { isFetching, items } = this.props.categories
-
-		let listItems
+		const { isFetching, items } = this.props.categories;
+		let listItems;
 
 		if ( items.length ) {
-			listItems = this.renderList()
+			listItems = this.renderList();
 		} else {
 			if ( isFetching ) {
-				listItems = this.renderSpinner()
+				listItems = this.renderSpinner();
 			} else {
-				listItems = this.renderNoCategories()
+				listItems = this.renderNoCategories();
 			}
 		}
 
@@ -49,13 +53,8 @@ class CategoryList extends React.Component {
 			<ul className="category-list">
 				{ listItems }
 			</ul>
-		)
+		);
 	}
 }
 
-CategoryList.propTypes = {
-	categories: PropTypes.object.isRequired,
-	dispatch:   PropTypes.func.isRequired
-}
-
-export default connect( state => ({ categories: state.categories }) )( CategoryList )
+export default connect( state => ({ categories: state.categories }) )( CategoryList );
