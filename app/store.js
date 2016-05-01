08700e20a0e45205ce02a027e0eec9e787d90408
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 21:14:59 +0800

    Add promiseMiddleware to store

--- app/store.js
+++ app/store.js
@@ -1,12 +1,33 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import reducers from './reducers';
+import thunk from 'redux-thunk';
+import reducers from 'reducers';
 
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
-)( createStore );
+function promiseMiddleware() {
+	return next => action => {
+		const { promise, type, ...rest } = action;
 
+		if ( ! promise ) return next( action );
+
+		const SUCCESS = `${type}_SUCCESS`;
+		const REQUEST = `${type}_REQUEST`;
+		const FAILURE = `${type}_FAILURE`;
+
+		next({ ...rest, type: REQUEST });
+
+		return promise
+			.then( req => {
+				next({ ...rest, req, type: SUCCESS });
+				return true;
+			})
+			.catch( error => {
+				next({ ...rest, error, type: FAILURE });
+				return false;
+			});
+	};
+}
+
+const createStoreWithMiddleware = applyMiddleware( thunk, promiseMiddleware )( createStore );
 const store = createStoreWithMiddleware( reducers );
 
 export default store;
