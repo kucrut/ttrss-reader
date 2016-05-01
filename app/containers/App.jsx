c861484ebc48fcb96d760f84f36498e949e45101
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-02 00:22:03 +0800

    Better doc title

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { sessionShape } from 'reducers/session';
 import { checkLastSession } from 'actions/session';
+import { getCount } from 'helpers';
 import SettingsForm from 'containers/SettingsForm';
 import SubscribeForm from 'containers/SubscribeForm';
 import LoginForm from 'containers/LoginForm';
@@ -10,11 +11,13 @@ import Sidebar from 'containers/Sidebar';
 import Main from 'containers/Main';
 import Alert from 'components/Alert';
 import Spinner from 'components/Spinner';
+import Helmet from 'react-helmet';
 
 
 class App extends React.Component {
 	static propTypes = {
 		session:           PropTypes.shape( sessionShape ).isRequired,
+		freshCount:        PropTypes.number.isRequired,
 		isSubscribing:     PropTypes.bool.isRequired,
 		isEditingSettings: PropTypes.bool.isRequired,
 		dispatch:          PropTypes.func.isRequired
@@ -31,15 +34,16 @@ class App extends React.Component {
 	}
 
 	render() {
-		const { dispatch, session, isEditingSettings, isSubscribing } = this.props;
+		const { dispatch, session, isEditingSettings, isSubscribing, freshCount } = this.props;
 		const { isChecked, isAsking, url, sid } = session;
-
+		let title = 'Tiny Tiny RSS Reader';
 		let content;
 
 		if ( url && sid ) {
 			if ( isEditingSettings ) {
 				content = (
 					<div className="whole-wrap">
+						<Helmet title={ `Settings « ${title}` } />
 						<SettingsForm />
 					</div>
 				);
@@ -47,12 +51,18 @@ class App extends React.Component {
 				if ( isSubscribing ) {
 					content = (
 						<div className="whole-wrap">
+							<Helmet title={ `Add Feed « ${title}` } />
 							<SubscribeForm />
 						</div>
 					);
 				} else {
+					if ( 0 < freshCount ) {
+						title = `(${getCount( freshCount )}) ${title}`;
+					}
+
 					content = (
 						<div className="whole-wrap">
+							<Helmet title={ title } />
 							<Header />
 							<Sidebar />
 							<Main />
@@ -67,6 +77,7 @@ class App extends React.Component {
 			} else {
 				content = (
 					<div className="whole-wrap">
+						<Helmet title={ `Login « ${title}` } />
 						<LoginForm dispatch={ dispatch } isAsking={ isAsking } />
 						<Alert />
 					</div>
@@ -81,6 +92,7 @@ class App extends React.Component {
 function mapStateToProps( state ) {
 	return {
 		session:           state.session,
+		freshCount:        state.categories.freshCount,
 		isSubscribing:     state.subscription.isOpen,
 		isEditingSettings: state.settings.isEditing
 	};
