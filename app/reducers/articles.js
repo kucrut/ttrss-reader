e27c8e3ff581c8484fd091839d006a21c58c4d42
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-01 14:16:50 +0800

    Lint

--- app/reducers/articles.js
+++ app/reducers/articles.js
@@ -1,8 +1,14 @@
-import * as _ from 'lodash'
-import { REQUESTED_LOGOUT } from '../actions/session'
-import { UPDATED_SETTINGS } from '../actions/settings'
-import { REQUESTED_ARTICLES, RECEIEVED_ARTICLES,
-	SELECTED_ARTICLE, UPDATED_ARTICLES, CLEARED_ARTICLES } from '../actions/articles'
+import { each, findIndex } from 'lodash';
+import { REQUESTED_LOGOUT } from 'actions/session';
+import { UPDATED_SETTINGS } from 'actions/settings';
+import {
+	REQUESTED_ARTICLES,
+	RECEIEVED_ARTICLES,
+	SELECTED_ARTICLE,
+	UPDATED_ARTICLES,
+	CLEARED_ARTICLES
+} from 'actions/articles';
+
 
 const initialState = {
 	feedId:       '',
@@ -11,10 +17,10 @@ const initialState = {
 	items:        [],
 	isFetching:   false,
 	hasMore:      false
-}
+};
 
 function normalizeArticle( article ) {
-	return Object.assign( {}, article, {
+	return Object.assign({}, article, {
 		id:      parseInt( article.id, 10 ),
 		score:   parseInt( article.score, 10 ),
 		feed_id: parseInt( article.feed_id, 10 )
@@ -22,91 +28,95 @@ function normalizeArticle( article ) {
 }
 
 export default function articles( state = initialState, action ) {
+	let newState;
+	let newItems;
+	let currentIndex;
+
 	switch ( action.type ) {
 		case REQUESTED_ARTICLES:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				isFetching: action.status
-			})
+			});
 
 		case RECEIEVED_ARTICLES:
-			state = Object.assign( {}, state, {
+			if ( action.feed.id !== state.feedId ) {
+				newState = Object.assign({}, initialState, {
+					feedId: action.feed.id
+				});
+			} else {
+				newState = Object.assign({}, state );
+			}
+
+			newState = Object.assign({}, newState, {
 				isFetching: false
 			});
 
-			if ( action.feed.id !== state.feedId ) {
-				state = Object.assign( {}, initialState, {
-					feedId: action.feed.id
-				})
-			}
-
 			if ( ! action.items || ! action.items.length ) {
-				return Object.assign( {}, state, {
+				return Object.assign({}, newState, {
 					hasMore: false
-				})
-			}
-
-			let allItems = state.items.concat( action.items.map( normalizeArticle ) )
-
-			state = Object.assign( {}, state, {
-				items:   allItems,
-				hasMore: action.hasMore
-			})
-
-			return state
-
-		case SELECTED_ARTICLE:
-			let currentIndex
-
-			if ( '' === action.currentId ) {
-				currentIndex = ''
-			} else {
-				currentIndex = state.items.findIndex( ( article, index ) => {
-					if ( action.currentId === article.id ) {
-						return article
-					}
 				});
 			}
 
-			return Object.assign( {}, state, {
-				currentId:    action.currentId,
-				currentIndex: currentIndex
-			})
+			return Object.assign({}, newState, {
+				items:   newState.items.concat( action.items.map( normalizeArticle ) ),
+				hasMore: action.hasMore
+			});
 
-		case UPDATED_ARTICLES:
-			state = Object.assign( {}, state, {
-				isFetching: false
-			})
+		case SELECTED_ARTICLE:
+			if ( '' === action.currentId ) {
+				currentIndex = '';
+			} else {
+				currentIndex = state.items.findIndex( ( article ) => {
+					let found;
 
-			if ( ! action.items || ! action.items.length ) {
-				return state
+					if ( action.currentId === article.id ) {
+						found = article;
+					}
+
+					return found;
+				});
 			}
 
-			let newItems = action.items.map( normalizeArticle )
+			return Object.assign({}, state, {
+				currentId: action.currentId,
+				currentIndex
+			});
 
-			_.each( newItems, ( item ) => {
-				let index = _.findIndex( state.items, { id: item.id } )
+		case UPDATED_ARTICLES:
+			newState = Object.assign({}, state, {
+				isFetching: false
+			});
+
+			if ( ! action.items || ! action.items.length ) {
+				return newState;
+			}
+
+			newItems = action.items.map( normalizeArticle );
+
+			each( newItems, ( item ) => {
+				const index = findIndex( newState.items, { id: item.id });
 
 				if ( -1 < index ) {
-					state.items[ index ] = item
+					newState.items[ index ] = item;
 				}
-			})
+			});
 
-			return state
+			return newState;
 
 		case CLEARED_ARTICLES:
-			return Object.assign( {}, state, {
+			return Object.assign({}, state, {
 				feedId:       state.feedId,
 				currentId:    '',
 				currentIndex: '',
 				items:        [],
 				hasMore:      false
-			})
+			});
 
 		case REQUESTED_LOGOUT:
 		case UPDATED_SETTINGS:
-			return Object.assign( {}, initialState )
+			return Object.assign({}, initialState );
 
 		default:
-			return state
+			return state;
 	}
 }
