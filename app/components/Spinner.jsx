f9ad9f5af9c40f3daff53f1bd30b7383629ebbfb
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-04 04:43:56 +0800

    CSS: Spinner

--- app/components/Spinner.jsx
+++ app/components/Spinner.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+
+import stlFa from 'css/common/fa';
+import stlElements from 'css/common/elements';
+const styles = Object.assign({}, stlFa, stlElements );
+const cx = classNames.bind( styles );
 
 export default function Spinner() {
+	const clsIcon = cx( ['fa-spinner', 'animate-spin'] );
+
 	return (
-		<i className="fa-spinner animate-spin" />
+		<i className={ clsIcon } />
 	);
 }
