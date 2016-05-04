58f47a652aae9a362392829792c544a6f634e5d3
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-04 08:19:59 +0800

    CSS: Load More Button

--- app/components/LoadMoreButton.jsx
+++ app/components/LoadMoreButton.jsx
@@ -1,7 +1,13 @@
 import React, { PropTypes } from 'react';
 import _ from 'lodash';
+import classNames from 'classnames/bind';
 import { fetchFeedArticles } from 'actions/articles';
 import Spinner from 'components/Spinner';
+
+import stlFa from 'css/common/fa';
+import stlElements from 'css/common/elements';
+import stlButton from 'css/components/load-more-button';
+const cx = classNames.bind( Object.assign({}, stlFa, stlButton ) );
 
 
 class LoadMoreButton extends React.Component {
@@ -37,19 +43,21 @@ class LoadMoreButton extends React.Component {
 	}
 
 	renderIcon() {
+		const clsIcon = cx( ['fa', 'fa-cw'] );
 		let element;
 
 		if ( this.props.articles.isFetching ) {
 			element = ( <Spinner /> );
 		} else {
-			element = ( <i className="fa-cw" /> );
+			element = ( <i className={ clsIcon } /> );
 		}
 
 		return element;
 	}
 
 	render() {
-		let icon  = this.renderIcon();
+		const icon    = this.renderIcon();
+		const clsWrap = cx( 'loadMoreWrap' );
 		let attrs;
 
 		if ( this.props.articles.isFetching ) {
@@ -59,12 +67,8 @@ class LoadMoreButton extends React.Component {
 		}
 
 		return (
-			<p className="load-more-wrap">
-				<button
-					{ ...attrs }
-					className="uk-button uk-button-large"
-					onClick={ this.handleClick }
-				>{ icon } Load More</button>
+			<p className={ clsWrap }>
+				<button { ...attrs } onClick={ this.handleClick }>{ icon } Load More</button>
 			</p>
 		);
 	}
