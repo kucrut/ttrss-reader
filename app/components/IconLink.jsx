465293e841458def9033089fd97933529e12264e
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-05 07:39:13 +0800

    IconLink: Accept extraClass

--- app/components/IconLink.jsx
+++ app/components/IconLink.jsx
@@ -2,9 +2,9 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames/bind';
 import styles from 'css/common/fa';
 
-export default function IconLink({ text, title, type, handler }) {
+export default function IconLink({ text, title, type, handler, extraClass }) {
 	const cx  = classNames.bind( styles );
-	const cls = cx( ['fa', `fa-${type}`] );
+	const cls = cx( ['fa', `fa-${type}`, extraClass] );
 
 	return (
 		<a onClick={ handler } title={ title } className={ cls }>{ text }</a>
@@ -12,8 +12,9 @@ export default function IconLink({ text, title, type, handler }) {
 }
 
 IconLink.propTypes = {
-	type:    PropTypes.string.isRequired,
-	text:    PropTypes.string,
-	title:   PropTypes.string,
-	handler: PropTypes.func
+	type:       PropTypes.string.isRequired,
+	text:       PropTypes.string,
+	title:      PropTypes.string,
+	handler:    PropTypes.func,
+	extraClass: PropTypes.string
 };
