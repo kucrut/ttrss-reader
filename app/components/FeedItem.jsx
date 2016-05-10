9034c3214d0278ed375e993c9db79cb814f9ecac
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-10 18:00:13 +0800

    FeedItem: Cleanup

--- app/components/FeedItem.jsx
+++ app/components/FeedItem.jsx
@@ -1,15 +1,11 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import classNames from 'classnames/bind';
 import { selectFeed } from 'actions/feeds';
 import { hideSidebar } from 'actions/ui';
 import { getCount } from 'helpers';
 import Icon from 'components/Icon';
+import styles from 'css/containers/category-list';
 
-import stlElements from 'css/common/elements';
-import stlCategoryList from 'css/containers/category-list';
-const styles = Object.assign({}, stlCategoryList, stlElements );
-const cx = classNames.bind( styles );
 
 class FeedItem extends React.Component {
 	static propTypes = {
@@ -48,14 +44,13 @@ class FeedItem extends React.Component {
 	render() {
 		const { feed, current } = this.props;
 		const isSelected = feed.id === current.id;
-		const clsName = cx( ['name', 'text-truncate'] );
-		const clsLink = cx({ current: isSelected });
+		const clsLink = isSelected ? 'current' : '';
 
 		return (
 			<li>
 				<a onClick={ this.handleClick } className={ clsLink }>
 					<Icon type="rss-squared" />
-					<span className={ clsName }>{ feed.title }</span>
+					<span className={ styles.name }>{ feed.title }</span>
 					{ this.renderCount() }
 				</a>
 			</li>
